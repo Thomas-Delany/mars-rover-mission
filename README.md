aa4ff4be1a896c36d66d2ee28a0c12960c293e60
thomas <info@englishreservoir.com>
2025-05-01 11:53:03 +0200

    no more fixed width for columns, instead opted for Bootstrap classes for easier responsiveness

--- README.md
+++ README.md
@@ -8,14 +8,19 @@ that are understood by the rover.
 
 Requirements
 
-● You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W)
+You are given the initial starting point (x,y) of a rover and the direction (N,S,E,W)
 it is facing.
-● The rover receives a collection of commands. (E.g.) FFRRFFFRL
-● The rover can move forward (f).
-● The rover can move left/right (l,r).
-● Suppose we are on a really weird planet that is square. 200x200 for example
-● Implement obstacle detection before each move to a new square. If a given
+
+The rover receives a collection of commands. (E.g.) FFRRFFFRL
+
+The rover can move forward (f).
+
+The rover can move left/right (l,r).
+
+Suppose we are on a really weird planet that is square. 200x200 for example
+
+Implement obstacle detection before each move to a new square. If a given
 sequence of commands encounters an obstacle, the rover moves up to the last
 possible point, aborts the sequence and reports the obstacle.
-Take into account
-● Rovers are expensive, make sure the software works as expected.
+
+Take into account Rovers are expensive, make sure the software works as expected.
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,32 +13,30 @@
     <h1 class="p-5 text-center fw-bold">Mars Rover Mission</h1>
 
     <div class="container mt-5">
-        <div class="row">
+        <div class="row d-flex justify-content-center">
             <!-- Column 1: The Grid -->
-            <div class="col-md-8">
-                @for ($y = 9; $y >= 0; $y--) {{-- Y-axis: top to bottom --}}
-                <div class="d-flex">
-                    @for ($x = 0; $x < 10; $x++) {{-- X-axis: left to right --}}
+            <div class="col-md-4 pb-5">
+                @for ($y = 9; $y >= 0; $y--)
+                <div class="row g-0"> {{-- no gap between cols --}}
+                    @for ($x = 0; $x < 10; $x++)
                         @php
                         $highlightedSquare=($x===3 && $y===4);
                         @endphp
-                        <div
-                        class="border {{ $highlightedSquare ? 'bg-dark' : '' }} cell"
-                        style="width: 40px; height: 40px;"
-                        data-x="{{ $x }}"
-                        data-y="{{ $y }}">
+                        <div class="col p-0 border {{ $highlightedSquare ? 'bg-dark' : '' }} cell ratio ratio-1x1"
+                        data-x="{{ $x }}" data-y="{{ $y }}">
+                        {{-- Optional: show coordinates --}}
+                        {{-- {{ $x }},{{ $y }} --}}
                 </div>
                 @endfor
             </div>
             @endfor
         </div>
 
-        <!-- Column 2: Controls -->
         <div class="col-md-4">
-            <h4 class="mb-3">Controls</h4>
-            <button class="btn btn-primary w-100 mb-2">Move Forward</button>
-            <button class="btn btn-secondary w-100 mb-2">Turn Left</button>
-            <button class="btn btn-secondary w-100 mb-2">Turn Right</button>
+            <h4 class="mb-3 text-center fs-2">Controls</h4>
+            <button class="btn btn-primary w-100 mb-4">Move Forward</button>
+            <button class="btn btn-secondary w-100 mb-4">Turn Left</button>
+            <button class="btn btn-secondary w-100 mb-4">Turn Right</button>
         </div>
     </div>
     </div>
